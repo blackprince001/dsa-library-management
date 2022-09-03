36efe040f92f75f30eac3c2a6a2bf3f2ec06a5d9
Blackprincex <appiahboaduprince@gmail.com>
2022-09-03 18:31:24 +0000

    fix: import fix for library class

--- manager/database/schemas/library.py
+++ manager/database/schemas/library.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 
 from manager.database.schemas.book import Book as BookSchema
-from manager.database.schemas.users import Admin as AdminSchema, Student as UserSchema
+from manager.database.schemas.users import Admin as AdminSchema, User as UserSchema
 
 
 class Library(BaseModel):
