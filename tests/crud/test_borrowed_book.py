6d16fc8e681e0665d698040245c93e78a5f71f2d
Blackprincex <appiahboaduprince@gmail.com>
2022-09-04 19:21:47 +0000

    tests: test_borrowed_book fail fix

--- tests/crud/test_borrowed_book.py
+++ tests/crud/test_borrowed_book.py
@@ -6,26 +6,30 @@ from manager.database.crud.borrowed_book import (
 )
 from manager.database.crud.book import create_book
 from manager.database.crud.author import AuthorCreate, create_author
+from manager.database.crud.user import create_user
 from manager.database.schemas.book import BorrowedBookCreate, BookCreate
 
-def test_create_borrowed_book(db, book):
-    author = create_author(
-        db=db,
-        author=AuthorCreate(name='King Sark'),
+
+def test_create_borrowed_book(db, book, author, user):
+    db_author = create_author(db=db, author=author)
+    db_user = create_user(db=db, user=user)
+    db_book = create_book(db=db, book=book, author_ids=[db_author.id])
+
+    db_borrowed_book = create_borrowed_book(
+        db=db, borrow_book=BorrowedBookCreate(user_id=db_user.id, book_id=db_book.id)
     )
 
-    db_book = create_book(db=db, book=book, author_ids=[author.id])
-
-    db_borrowed_book = create_borrowed_book(db, db_book)
-
-    assert db_borrowed_book.user_id == book.id
+    assert db_borrowed_book.book_id == db_book.id
+    assert db_borrowed_book.user_id == db_user.id
 
 
 def test_removed_book():
     pass
 
+
 def test_borrowed_books():
     pass
+
 
 def test_borrowed_books_admin():
     pass
